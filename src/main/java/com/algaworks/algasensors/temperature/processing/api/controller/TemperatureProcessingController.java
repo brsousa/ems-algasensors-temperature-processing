885061bd7a0b66702749c136f057cb484943194a
Bruno Augusto <brunoaugusto.segme@gmail.com>
2025-12-18 09:14:30 -0300

    Testing use of headers in rabbitmq messages

--- src/main/java/com/algaworks/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
+++ src/main/java/com/algaworks/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
@@ -5,6 +5,8 @@ import com.algaworks.algasensors.temperature.processing.common.IdGenerator;
 import io.hypersistence.tsid.TSID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -38,7 +40,12 @@ public class TemperatureProcessingController {
         String routingKey = "";
         Object payload = logOutput;
 
-        rabbitTemplate.convertAndSend(exchange, routingKey, payload);
+        MessagePostProcessor messagePostProcessor = (MessagePostProcessor) message -> {
+            message.getMessageProperties().setHeader("sensorId", logOutput.getSensorId().toString());
+            return message;
+        };
+
+        rabbitTemplate.convertAndSend(exchange, routingKey, payload, messagePostProcessor);
     }
 
     private static TemperatureLogOutput buildOutputResponse(TSID sensorId, Double temperature) {
